9f92888d50617e24433b30c16d0b6cb35c6c3202
brianjrok <brianjrok@gmail.com>
2022-07-12 20:01:35 -0500

    initial firebase setup

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+// Your web app's Firebase configuration
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyAOA-9bvwphP46ox6WIBtKW8PlFSiPTvTs",
+  authDomain: "where-s-waldo-7306f.firebaseapp.com",
+  projectId: "where-s-waldo-7306f",
+  storageBucket: "where-s-waldo-7306f.appspot.com",
+  messagingSenderId: "132299956344",
+  appId: "1:132299956344:web:fe68ba17cfc153b2200d8d"
+});
+
+export default app;
