344c98e9f46d4acb746a043e98e9d7ae013e13b3
brianjrok <brianjrok@gmail.com>
2022-07-14 23:00:57 -0500

    push to ask help on Odin discord

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-
 import Dropdown from './Dropdown';
+import db from '../firebase/firebase-config';
+// import { doc, getDoc, onSnapshot, collection } from 'firebase/firestore';
 
 import imgURL from '../assets/WaldoBanner.jpg';
 
@@ -11,6 +12,18 @@ const Image = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
   const [image] = useState(imgURL);
+
+  // const [characterCoords, setCharacterCoords] = useState([]);
+
+  // useEffect(() => {
+  // }, [])
+  
+  // const fetchData = async() => {
+  //   const docRef = doc(db, 'characterCoords', 'I6QASwXyjQ8i6XlirwD4');
+  //   const docSnap = await getDoc(docRef);
+  //   console.log(docSnap.data());
+  //   return docSnap.data();
+  // }
 
   const handleMouseMove = (e) => {
     setCoords({
--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,13 +1,19 @@
-import firebase from 'firebase/app';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
 // Your web app's Firebase configuration
-const app = firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyAOA-9bvwphP46ox6WIBtKW8PlFSiPTvTs",
   authDomain: "where-s-waldo-7306f.firebaseapp.com",
   projectId: "where-s-waldo-7306f",
   storageBucket: "where-s-waldo-7306f.appspot.com",
   messagingSenderId: "132299956344",
   appId: "1:132299956344:web:fe68ba17cfc153b2200d8d"
-});
+};
 
-export default app;
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app);
+
+console.log(getDocs(collection(db, 'characterCoords')));
+
+export default db;
